b2777baef4c555fdb415826e5f5f04367081efd6
jerslf <jerslf@hotmail.co.uk>
2025-04-12 15:59:41 +0200

    Complete Generate Pges Recursively

--- src/gencontent.py
+++ src/gencontent.py
@@ -26,6 +26,22 @@ def generate_page(from_path, template_path, dest_path):
     to_file.write(template)
 
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for entry in os.listdir(dir_path_content):
+        content_path = os.path.join(dir_path_content, entry)
+        dest_path = os.path.join(dest_dir_path, entry)
+
+        if os.path.isfile(content_path) and content_path.endswith(".md"):
+            # Replace .md with .html
+            dest_file_path = os.path.splitext(dest_path)[0] + ".html"
+            os.makedirs(os.path.dirname(dest_file_path), exist_ok=True)
+
+            generate_page(content_path, template_path, dest_file_path)
+
+        elif os.path.isdir(content_path):
+            generate_pages_recursive(content_path, template_path, dest_path)
+
+
 def extract_title(md):
     lines = md.split("\n")
     for line in lines:
